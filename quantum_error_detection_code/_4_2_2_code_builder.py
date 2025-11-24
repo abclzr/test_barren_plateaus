0fdb509dcc4e1a3a2df7a514c8427bbab71c9150
abclzr <dedicactus545@gmail.com>
2025-11-24 01:08:37 -0500

    finished [[4, 2, 2]] code block builder

--- quantum_error_detection_code/_4_2_2_code_builder.py
+++ quantum_error_detection_code/_4_2_2_code_builder.py
@@ -22,6 +22,11 @@ class ClassicalRegisterAllocator:
     def get_clbit_index(self):
         self.cnt += 1
         return self.cnt - 1
+    
+    def copy(self):
+        new_allocator = ClassicalRegisterAllocator(self.clibts)
+        new_allocator.cnt = self.cnt
+        return new_allocator
 
 
 class _4_2_2_Code_Builder:
@@ -34,6 +39,7 @@ class _4_2_2_Code_Builder:
         self.a1 = a1
         self.a2 = a2
         self.clbit_allocator = clbit_allocator
+        self.syndrome_bits = []
 
     def initialize(self):
         syndrome_bit_index = self.clbit_allocator.get_clbit_index()
@@ -45,12 +51,14 @@ class _4_2_2_Code_Builder:
         self.base_circuit.cx(self.t, self.a1)
         self.base_circuit.cx(self.b, self.a1)
         self.base_circuit.measure(self.a1, syndrome_bit_index)
+        self.syndrome_bits.append(syndrome_bit_index)
     
     def transversal_Hadamard(self):
         self.base_circuit.h(self.t)
         self.base_circuit.h(self.b)
         self.base_circuit.h(self.q1)
         self.base_circuit.h(self.q2)
+        self.base_circuit.swap(self.q1, self.q2)
     
     def transversal_CNOT(self, other_code_builder):
         self.base_circuit.cx(self.t, other_code_builder.t)
@@ -63,6 +71,13 @@ class _4_2_2_Code_Builder:
     
     def logical_RX(self, theta: float, qubit: int):
         self.base_circuit.rxx(theta, self.t, self.q1 if qubit == 1 else self.q2)
+    
+    def logical_RZ(self, theta: float, qubit: int):
+        self.base_circuit.rzz(theta, self.b, self.q1 if qubit == 1 else self.q2)
+
+    def logical_X(self, qubit: int):
+        self.base_circuit.x(self.t)
+        self.base_circuit.x(self.q1 if qubit == 1 else self.q2)
 
     def syndrome_measurement(self):
         syndrome_bit_index1 = self.clbit_allocator.get_clbit_index()
@@ -84,6 +99,8 @@ class _4_2_2_Code_Builder:
         self.base_circuit.h(self.a2)
         self.base_circuit.measure(self.a1, syndrome_bit_index1)
         self.base_circuit.measure(self.a2, syndrome_bit_index2)
+        self.syndrome_bits.append(syndrome_bit_index1)
+        self.syndrome_bits.append(syndrome_bit_index2)
     
     def measurement(self) -> list[int]:
         syndrome_bit_index1 = self.clbit_allocator.get_clbit_index()
@@ -104,19 +121,29 @@ class _4_2_2_Code_Builder:
         
         self.base_circuit.measure(self.a1, syndrome_bit_index1)
         self.base_circuit.measure(self.a2, syndrome_bit_index2)
+        self.syndrome_bits.append(syndrome_bit_index1)
+        self.syndrome_bits.append(syndrome_bit_index2)
         
-        cbits = [self.clbit_allocator.get_clbit_index() for _ in range(4)]
-        self.base_circuit.measure(self.t, cbits[0])
-        self.base_circuit.measure(self.q1, cbits[1])
-        self.base_circuit.measure(self.q2, cbits[2])
-        self.base_circuit.measure(self.b, cbits[3])
-        return cbits
+        self.cbits = [self.clbit_allocator.get_clbit_index() for _ in range(4)]
+        self.base_circuit.measure(self.t, self.cbits[0])
+        self.base_circuit.measure(self.q1, self.cbits[1])
+        self.base_circuit.measure(self.q2, self.cbits[2])
+        self.base_circuit.measure(self.b, self.cbits[3])
+        return self.cbits
     
     def decode(self, bitstring: str) -> str:
-        assert len(bitstring) == 4
-        if bitstring.count('1') % 2 == 1:
+        for syndrome_bit_index in self.syndrome_bits:
+            if bitstring[-syndrome_bit_index] == '1':
+                return 'invalid'
+        measured_bitstring = ''.join([bitstring[-self.cbits[0]], bitstring[-self.cbits[1]], bitstring[-self.cbits[2]], bitstring[-self.cbits[3]]])
+        if measured_bitstring.count('1') % 2 == 1:
             return 'invalid'
-        if bitstring[-1] == '0':
-            return bitstring[2:4]
+        if measured_bitstring[3] == '0':
+            return measured_bitstring[1:3]
         else:
-            return ''.join(['1' if bit == '0' else '0' for bit in bitstring[2:4]])
+            return ''.join(['1' if bit == '0' else '0' for bit in measured_bitstring[1:3]])
+    
+    def copy_for_a_new_circuit(self, new_base_circuit: QuantumCircuit):
+        new_builder = _4_2_2_Code_Builder(new_base_circuit, self.t, self.b, self.q1, self.q2, self.a1, self.a2, self.clbit_allocator.copy())
+        new_builder.syndrome_bits = self.syndrome_bits.copy()
+        return new_builder
---
+++ quantum_error_detection_code/evaluate_H2.py
@@ -0,0 +1,217 @@
+import pdb
+import pickle
+import os
+import numpy as np
+import time
+
+from qiskit_nature.second_q.drivers import PySCFDriver
+from qiskit_nature.second_q.mappers import JordanWignerMapper, BravyiKitaevMapper, ParityMapper
+from qiskit_nature.second_q.circuit.library import UCCSD, HartreeFock
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer.noise import (NoiseModel, QuantumError, ReadoutError,
+    pauli_error, depolarizing_error, thermal_relaxation_error)
+
+from qiskit_aer import AerSimulator
+from qiskit_aer.quantum_info import AerStatevector
+
+from scipy.optimize import minimize
+from qiskit_ibm_runtime import QiskitRuntimeService, Session
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+from _4_2_2_code_builder import _4_2_2_Code_Builder, ClassicalRegisterAllocator
+
+noise_level = 1
+noise_model = NoiseModel()
+# noise_model.add_all_qubit_quantum_error(depolarizing_error(0.0004 * noise_level, 1), ['u1', 'u2', 'u3', 's', 'x', 'sdg', 'rx', 'ry', 'rz'])
+# noise_model.add_all_qubit_quantum_error(depolarizing_error(0.003 * noise_level, 2), ['rzz', 'rxx', 'ryy'])
+# noise_model.add_all_qubit_readout_error(ReadoutError([[1 - 0.003 * noise_level, 0.003 * noise_level], [0.003 * noise_level, 1 - 0.003 * noise_level]]))
+
+backend = AerSimulator(noise_model=noise_model, shots=10000)
+mole_name = 'H2'
+mapper_name = 'jordan_wigner'
+ansatz_name = 'UCCSD'
+reps = 3
+os.makedirs("training_data", exist_ok=True)
+if ansatz_name == 'HWPA':
+    filename = f"../training_data/{mole_name}_{mapper_name}_{ansatz_name}_{reps}.pickle"
+elif ansatz_name == 'UCCSD':
+    filename = f"../training_data/{mole_name}_{mapper_name}_{ansatz_name}.pickle"
+# Load result_cobyla
+with open(filename, "rb") as f:
+    result_cobyla = pickle.load(f)
+
+print(f"Loaded optimization result from {filename}")
+driver_dict = {
+    'H2': PySCFDriver(atom="H .0 .0 .0; H .0 .0 0.735", basis='sto3g'),
+    'LiH': PySCFDriver(atom="Li .0 .0 .0; H .0 .0 1.5699", basis='sto3g'),
+    'BeH2': PySCFDriver(atom="Be .0 .0 .0; H .0 .0 -1.3; H .0 .0 1.3", basis='sto3g'),
+    'CH4': PySCFDriver(atom="C .0 .0 .0; H .0 .0 1.0; H .0 .0 -1.0; H .0 1.0 .0; H .0 -1.0 .0", basis='sto3g'),
+    'MgH2': PySCFDriver(atom="Mg .0 .0 .0; H .0 .0 -1.3; H .0 .0 1.3", basis='sto3g'),
+    'LiCl': PySCFDriver(atom="Li .0 .0 .0; Cl .0 .0 -1.5", basis='sto3g'),
+    'CO2': PySCFDriver(atom="C .0 .0 .0; O .0 .0 1.0; O .0 .0 -1.0", basis='sto3g')
+}
+mapper_dict = {
+    'jordan_wigner': JordanWignerMapper(),
+    'bravyi_kitaev': BravyiKitaevMapper(),
+    'parity': ParityMapper()
+}
+driver = driver_dict[mole_name]
+problem = driver.run()
+mapper = mapper_dict[mapper_name]
+second_q_op = problem.hamiltonian.second_q_op()
+qubit_op_before_reduction = mapper.map(second_q_op)
+
+paulis = []
+coeffs = []
+for pauli, coeff in qubit_op_before_reduction.label_iter():
+    paulis.append(pauli)
+    coeffs.append(coeff.real)
+
+n_qubits = len(paulis[0])
+# train_ansatz(hamiltonian=qubit_op_before_reduction, ansatz=ansatz)
+
+ansatz = UCCSD(
+    problem.num_spatial_orbitals,
+    problem.num_particles,
+    mapper,
+    initial_state=HartreeFock(
+        problem.num_spatial_orbitals,
+        problem.num_particles,
+        mapper,
+    ),
+)
+param2 = result_cobyla.x[2]
+
+circ = QuantumCircuit(12, 12)
+clbit_allocator = ClassicalRegisterAllocator(12)
+block1_builder = _4_2_2_Code_Builder(circ, 0, 1, 2, 3, 8, 9, clbit_allocator)
+block2_builder = _4_2_2_Code_Builder(circ, 4, 5, 6, 7, 10, 11, clbit_allocator)
+block1_builder.initialize()
+block2_builder.initialize()
+block1_builder.logical_X(1)
+block2_builder.logical_X(1)
+
+double_excitation_ansatz_operator = ansatz.operators[2]
+for pauli_coeff_pair in double_excitation_ansatz_operator:
+    pauli = pauli_coeff_pair.paulis[0]
+    coeff = pauli_coeff_pair.coeffs[0]
+    block1_builder.transversal_Hadamard()
+    block2_builder.transversal_Hadamard()
+    if pauli[-1] == 'Y':
+        block1_builder.logical_RX(-np.pi / 2, 1)
+    if pauli[-2] == 'Y':
+        block1_builder.logical_RX(-np.pi / 2, 2)
+    if pauli[-3] == 'Y':
+        block2_builder.logical_RX(-np.pi / 2, 1)
+    if pauli[-4] == 'Y':
+        block2_builder.logical_RX(-np.pi / 2, 2)
+    block1_builder.transversal_CNOT(block2_builder)
+    block2_builder.logical_RZZ(coeff * 2 * param2)
+    block1_builder.transversal_CNOT(block2_builder)
+    if pauli[-1] == 'Y':
+        block1_builder.logical_RX(np.pi / 2, 1)
+    if pauli[-2] == 'Y':
+        block1_builder.logical_RX(np.pi / 2, 2)
+    if pauli[-3] == 'Y':
+        block2_builder.logical_RX(np.pi / 2, 1)
+    if pauli[-4] == 'Y':
+        block2_builder.logical_RX(np.pi / 2, 2)
+    block1_builder.transversal_Hadamard()
+    block2_builder.transversal_Hadamard()
+
+final_expval = 0
+for pauli, coeff in zip(paulis, coeffs):
+    circuit_with_measure_basis = QuantumCircuit(circ.num_qubits, circ.num_clbits)
+    circuit_with_measure_basis = circuit_with_measure_basis.compose(circ)
+    block1_builder_with_measure_basis = block1_builder.copy_for_a_new_circuit(circuit_with_measure_basis)
+    block2_builder_with_measure_basis = block2_builder.copy_for_a_new_circuit(circuit_with_measure_basis)
+    map_codeblock_and_qubit = {
+        0:(block1_builder_with_measure_basis, 1),
+        1:(block1_builder_with_measure_basis, 2),
+        2:(block2_builder_with_measure_basis, 1),
+        3:(block2_builder_with_measure_basis, 2)
+    }
+    for i, p in enumerate(pauli[::-1]):
+        if p == 'I' or p == 'Z':
+            continue
+        elif p == 'X':
+            builder, qubit = map_codeblock_and_qubit[i]
+            builder.logical_RZ(np.pi/2, qubit)
+            builder.logical_RX(np.pi/2, qubit)
+        elif p == 'Y':
+            builder, qubit = map_codeblock_and_qubit[i]
+            builder.logical_RX(np.pi/2, qubit)
+    cbits1 = block1_builder_with_measure_basis.measurement()
+    cbits2 = block2_builder_with_measure_basis.measurement()
+
+    def analyze_counts(counts, pauli):
+        expval = 0
+        sum_total = 0
+        sum_discard = 0
+        sum_keep = 0
+        for bitstring, count in counts.items():
+            bit_val = 1
+            # print("Bitstring:", bitstring)
+            decode1 = block1_builder_with_measure_basis.decode(bitstring)
+            if decode1 == 'invalid':
+                print(bitstring)
+                sum_discard += count
+                continue
+            decode2 = block2_builder_with_measure_basis.decode(bitstring)
+            if decode2 == 'invalid':
+                print(bitstring)
+                sum_discard += count
+                continue
+            decode_bitstring = decode1 + decode2
+            for i, p in enumerate(pauli[::-1]):
+                if p == 'I':
+                    continue
+                elif p == 'X' or p == 'Y' or p == 'Z':
+                    if decode_bitstring[i] == '1':
+                        bit_val *= -1
+            expval += bit_val * count
+            sum_keep += count
+        expval /= sum_keep
+        return expval, sum_keep, sum_discard
+    result = backend.run(circuit_with_measure_basis).result()
+    expval, sum_keep, sum_discard = analyze_counts(result.get_counts(), pauli)
+    final_expval += expval * coeff
+    print(f"Pauli: {pauli}, Coeff: {coeff}, Expval: {expval} Shots kept: {sum_keep}, Shots discarded: {sum_discard}")
+
+print(f"Final expval: {final_expval}")
+exit()
+def objective_function(param_values, noise_model=None):
+    # return ansatz.evaluate_objective_function(param_values)
+    return ansatz.evaluate_objective_function_with_noise(param_values, noise_model)
+
+# Convert parameters to a numpy array for optimization
+initial_params = np.array([0. for param in ansatz.parameters()])
+
+# Define a wrapper for the optimizer
+def scipy_objective(params):
+    param_dict = dict(zip(ansatz.parameters(), params))
+    ret = objective_function(param_dict)
+    print(f"time: {time.strftime('%Y-%m-%d %H:%M:%S')}, Current parameters: {params}, Objective function value: {ret}")
+    return ret
+
+# print(f'Hartree Fock Energy: {scipy_objective(initial_params)}')
+
+print(f'Nuclear repulsion energy: {problem.nuclear_repulsion_energy}')
+
+# Update map_param_values with optimized parameters from COBYLA
+optimized_params_cobyla = dict(zip(ansatz.parameters(), result_cobyla.x))
+print("COBYLA Optimized Parameters:", optimized_params_cobyla)
+results = []
+for noise_level in [1e-3, 1e-2, 1e-1, 1]:
+    noise_model = NoiseModel()
+    noise_model.add_all_qubit_quantum_error(depolarizing_error(0.0004 * noise_level, 1), ['u1', 'u2', 'u3', 's', 'x', 'sdg', 'rx', 'ry', 'rz'])
+    noise_model.add_all_qubit_quantum_error(depolarizing_error(0.003 * noise_level, 2), ['rzz', 'rxx', 'ryy'])
+    noise_model.add_all_qubit_readout_error(ReadoutError([[1 - 0.003 * noise_level, 0.003 * noise_level], [0.003 * noise_level, 1 - 0.003 * noise_level]]))
+    # noise_model.add_all_qubit_quantum_error(depolarizing_error(0.0004 * noise_level, 1), ['u1', 'u2', 'u3', 'h', 's', 'x', 'sdg', 'rx', 'ry', 'rz'])
+    # noise_model.add_all_qubit_quantum_error(depolarizing_error(0.003 * noise_level, 2), ['cx', 'cz', 'rzz', 'rxx', 'ryy'])
+    # noise_model.add_all_qubit_readout_error(ReadoutError([[1 - 0.003 * noise_level, 0.003 * noise_level], [0.003 * noise_level, 1 - 0.003 * noise_level]]))
+    value = objective_function(optimized_params_cobyla, noise_model)
+    print("COBYLA Optimized Objective Function Value:", value)
+    results.append((noise_level, value))
+print(results)
+with open("output.txt", "w") as f:
+    f.write(str(results))
